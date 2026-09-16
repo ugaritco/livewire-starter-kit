4414479c98db5106693bd39589f8afd2a26f013f
Muath R Abu Ouda <muathrabuouda@hotmail.com>
2026-09-17 00:40:02 +0300

    fix: update dependencies and remove laravel npm aliases

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
             fonts: [
                 bunny('Instrument Sans', {
                     weights: [400, 500, 600],
+                    optimizedFallbacks: false,
                 }),
             ],
         }),
